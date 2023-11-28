025e7d55809c587b1447f46818810c612f6f6bf4
poli14na <146544476+poli14na@users.noreply.github.com>
2023-11-28 22:38:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ import javax.swing.*;
 
 public class Main {
      public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ListManagementApp();
-            }
-        });
+              ListManagementApp listManagementApp = new ListManagementApp();
     }
 }
